75a48b10ff908f2e337966b7a625f692100c7eb3
HatDu <nianmaodu@gmail.com>
2019-08-28 20:20:03 +0800

    fix bug

--- tools/cal_sensmap.py
+++ tools/cal_sensmap.py
@@ -16,11 +16,13 @@ args = parser.parse_args()
 fnames = sorted(glob.glob(args.src+'/*'))
 total_time = 0
 for name in fnames:
+    n = os.path.split(name)[-1]
     target_path = os.path.join(args.dest, n)
     if os.path.exists(target_path):
+        print('skip %s', name)
         continue
     with h5py.File(name, 'r') as data:
-        n = os.path.split(name)[-1]
+        
         kspace = data['kspace']
         start = time.time()
         sense_maps = [bart.bart(1, f'ecalib -d0 -m1', kspace[i]) for i in range(kspace.shape[0])]
