06958603d22b45b7e4dc2697a8c4062c46337dc2
HatDu <nianmaodu@gmail.com>
2019-08-29 11:10:57 +0800

    add dilated_unet.py

--- core/models/unet/__init__.py
+++ core/models/unet/__init__.py
@@ -1,3 +1,10 @@
-from .baseline_unet import UnetModel
-def import_model(id):
-    return UnetModel
+
+def import_model(model_id):
+    if model_id == 0:
+        from .baseline_unet import UnetModel
+        return UnetModel
+    elif model_id == 1:
+        from .dilated_unet import UnetModel
+        return UnetModel
+    else:
+        raise NotImplementedError
