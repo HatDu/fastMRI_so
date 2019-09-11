c091a641e0f1155875d7c0d4108712509dbc8ce1
HatDu <nianmaodu@gmail.com>
2019-09-11 16:25:33 +0800

    fix names

--- ie.sh
+++ ie.sh
@@ -1,7 +1,7 @@
 # infer and eval
 
-cfg_file='configs/baseline_unet.py'
-ckpt='log/baseline_unet_20/best_model.pt'
+cfg_file='configs/complex_unet.py'
+ckpt='log/complex_unet_20/best_model.pt'
 device='0,1'
 
 # # visualize sensity map
@@ -10,14 +10,14 @@ device='0,1'
 # #     -a x4 -acq both 
 
 # val on 20 x4
-# rm -r data/infer/*.h5
-# CUDA_VISIBLE_DEVICES=$device python infer.py --cfg $cfg_file -c $ckpt \
-#     -i data/multicoil_val/ -o data/infer \
-#     -a x4 -acq both 
-# python eval.py --target-path data/multicoil_val/ --predictions-path data/infer \
-#     --challenge multicoil --acquisition CORPD_FBK
-# python eval.py --target-path data/multicoil_val/ --predictions-path data/infer \
-#     --challenge multicoil --acquisition CORPDFS_FBK
+rm -r data/infer/*.h5
+CUDA_VISIBLE_DEVICES=$device python infer.py --cfg $cfg_file -c $ckpt \
+    -i data/multicoil_val/ -o data/infer \
+    -a x4 -acq both 
+python eval.py --target-path data/multicoil_val/ --predictions-path data/infer \
+    --challenge multicoil --acquisition CORPD_FBK
+python eval.py --target-path data/multicoil_val/ --predictions-path data/infer \
+    --challenge multicoil --acquisition CORPDFS_FBK
 
 # val on 20 x8
 rm -r data/infer/*.h5
